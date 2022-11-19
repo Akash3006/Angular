889db4a30e4ed0d161ad3a23489d7de5ef41a698
Akash Repos <you@example.com>
2022-11-19 18:05:17 +0530

    router link attribute added(template level routing)

--- Client/src/app/app-routing.module.ts
+++ Client/src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { ListsComponent } from './lists/lists.component';
 import { MemberDetailsComponent } from './members/member-details/member-details.component';
 import { MembersListComponent } from './members/members-list/members-list.component';
 import { MessagesComponent } from './messages/messages.component';
@@ -9,6 +10,7 @@ const routes: Routes = [
   {path:'',component:HomeComponent},
   {path:'members',component:MembersListComponent},
   {path:'member/:id',component:MemberDetailsComponent},
+  {path:'lists',component:ListsComponent},
   {path:'messages',component:MessagesComponent},
   {path:'**',component:HomeComponent}
 ];
